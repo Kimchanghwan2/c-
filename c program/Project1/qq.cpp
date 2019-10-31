a27bc27e0d33ec0bc849fc8e66913f933a7a6cd2
user <user@DESKTOP-3DNUHQ9>
2019-10-31 14:24:19 +0900

    5장 연습문제 2번

--- c program/Project1/qq.cpp
+++ c program/Project1/qq.cpp
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 int main(void)
+
 {
-	double c1, c2, c3;
-	double f1, f2, f3;
-	printf("섭씨온도 입력하세요\n ");
-	scanf_s("%lf", &c1);
-	scanf_s("%lf", &c2);
-	scanf_s("%lf", &c3);
-	f1 = (9.0 / 5.0) * c1 + 32.0;
-	f2 = (9.0 / 5.0) * c2 + 32.0;
-	f3 = (9.0 / 5.0) * c3 + 32.0;
-	printf("화씨온도\n");
-	printf("%.4f\n", f1);
-	printf("%.4f\n", f2);
-	printf("%.4f\n", f3);
+	double x, y;
+	printf(" kg를 입력하시오.\n");
+	scanf_s("%lf", &x);
+
+	y = x * 2205 / 1000;
+	printf("%.3lf 파운드 입니다.\n", y);
+
 
 	return 0;
 }
+
+
+
--- c program/Project1/소스.cpp
+++ c program/Project1/소스.cpp
@@ -1,24 +1,47 @@
 #include <stdio.h>
 
+
+
 int main(void)
 
+
+
 {
+
 	int x, y;
 
+
+
 	printf("두 정수를 입력하시오.\n");
-	
-		scanf_s("%d %d", &x, & y);
-		if (x > y)
-		{
-			printf("큰 수를 작은 수로 나눈 몫 : %d\n", x / y);
-			printf("나머지 : %d\n", x % y);
-		}
-		if (y > x)
-		{
-			printf("큰 수를 작은 수로 나눈 몫 : %d\n", y / x);
-			printf("나머지 : %d\n", y % x);
-		}
-		
-	
-		return 0;
+
+
+
+	scanf_s("%d %d", &x, &y);
+
+	if (x > y)
+
+	{
+
+		printf("큰 수를 작은 수로 나눈 몫 : %d\n", x / y);
+
+		printf("나머지 : %d\n", x % y);
+
+	}
+
+	if (y > x)
+
+	{
+
+		printf("큰 수를 작은 수로 나눈 몫 : %d\n", y / x);
+
+		printf("나머지 : %d\n", y % x);
+
+	}
+
+
+
+
+
+	return 0;
+
 }
